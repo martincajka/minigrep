7916af32d999f9bdc5043d139ac8cf975a4e4d61
Martin Cajka <martin.cajka125@gmail.com>
2024-04-22 15:44:25 +0200

    refactoring using external libs

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,52 @@
-use std::{env, process};
+use anyhow::{Context, Result};
+use clap::Parser;
+use log::info;
 
-use minigrep::Config;
+fn main() -> Result<()> {
+    env_logger::init();
+    info!("Starting up");
+    let args = Cli::parse();
 
-fn main() {
-    let config = Config::build(env::args()).unwrap_or_else(|err| {
-        eprintln!("problem parsing argument: {err}");
-        process::exit(1);
-    });
+    info!("pattern: {:?}, path: {:?}", args.pattern, args.path);
 
-    if let Err(e) = minigrep::run(config) {
-        eprintln!("Application error: {e}");
-        process::exit(1);
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|| format!("Error reading `{:?}`", &args.path))?;
+
+    find_matches(&content, &args.pattern, &mut std::io::stdout())
+        .with_context(|| format!("Error querying the file `{:?}`", &args.path))?;
+    Ok(())
+}
+
+fn find_matches(
+    content: &str,
+    pattern: &str,
+    mut writer: impl std::io::Write,
+) -> Result<(), std::io::Error> {
+    for line in content.lines() {
+        if line.contains(pattern) {
+            return writeln!(writer, "{}", line);
+        }
+    }
+    Ok(())
+}
+
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io;
+
+    use crate::find_matches;
+
+    #[test]
+    fn find_a_match() -> io::Result<()> {
+        let mut result = Vec::new();
+        find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result)?;
+        assert_eq!(result, b"lorem ipsum\n");
+        Ok(())
     }
 }
