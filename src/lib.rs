dd11143e6ff822891db5500b34203a1f95c241c8
Martin Cajka <martin.cajka125@gmail.com>
2024-04-23 15:10:16 +0200

    split code to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,92 +1,35 @@
-use std::error::Error;
-use std::{env, fs};
+use clap::Parser;
 
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let content = fs::read_to_string(config.file_path)?;
-
-    let results = if config.ignore_case {
-        search_case_insensitive(&config.query, &content)
-    } else {
-        search(&config.query, &content)
-    };
-    for line in results {
-        println!("{line}")
+pub fn find_matches(
+    content: &str,
+    pattern: &str,
+    mut writer: impl std::io::Write,
+) -> Result<(), std::io::Error> {
+    for line in content.lines() {
+        if line.contains(pattern) {
+            return writeln!(writer, "{}", line);
+        }
     }
-
     Ok(())
 }
 
-pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    contents
-        .lines()
-        .filter(|line| line.contains(query))
-        .collect()
-}
-
-pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let case_insensitive_query = query.to_lowercase();
-    contents
-        .lines()
-        .filter(|line| line.to_lowercase().contains(&case_insensitive_query))
-        .collect()
-}
-
-pub struct Config {
-    pub query: String,
-    pub file_path: String,
-    pub ignore_case: bool,
-}
-impl Config {
-    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
-        args.next();
-
-        let query = match args.next() {
-            Some(arg) => arg,
-            None => return Err("Didn't get a query string"),
-        };
-
-        let file_path = match args.next() {
-            Some(arg) => arg,
-            None => return Err("Didn't get file path"),
-        };
-
-        let ignore_case = env::var("IGNORE_CASE").is_ok();
-
-        Ok(Config {
-            query,
-            file_path,
-            ignore_case,
-        })
-    }
+#[derive(Parser)]
+pub struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use std::io;
+
+    use crate::find_matches;
 
     #[test]
-    fn one_result() {
-        let query = "duct";
-        let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.";
-
-        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
-    }
-
-    #[test]
-    fn case_insensitive() {
-        let query = "rUsT";
-        let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Trust me.";
-
-        assert_eq!(
-            vec!["Rust:", "Trust me."],
-            search_case_insensitive(query, contents)
-        );
+    fn find_a_match() -> io::Result<()> {
+        let mut result = Vec::new();
+        find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result)?;
+        assert_eq!(result, b"lorem ipsum\n");
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,48 +5,14 @@ use log::info;
 fn main() -> Result<()> {
     env_logger::init();
     info!("Starting up");
-    let args = Cli::parse();
+    let args = minigrep::Cli::parse();
 
     info!("pattern: {:?}, path: {:?}", args.pattern, args.path);
 
     let content = std::fs::read_to_string(&args.path)
         .with_context(|| format!("Error reading `{:?}`", &args.path))?;
 
-    find_matches(&content, &args.pattern, &mut std::io::stdout())
+    minigrep::find_matches(&content, &args.pattern, &mut std::io::stdout())
         .with_context(|| format!("Error querying the file `{:?}`", &args.path))?;
     Ok(())
-}
-
-fn find_matches(
-    content: &str,
-    pattern: &str,
-    mut writer: impl std::io::Write,
-) -> Result<(), std::io::Error> {
-    for line in content.lines() {
-        if line.contains(pattern) {
-            return writeln!(writer, "{}", line);
-        }
-    }
-    Ok(())
-}
-
-#[derive(Parser)]
-struct Cli {
-    pattern: String,
-    path: std::path::PathBuf,
-}
-
-#[cfg(test)]
-mod tests {
-    use std::io;
-
-    use crate::find_matches;
-
-    #[test]
-    fn find_a_match() -> io::Result<()> {
-        let mut result = Vec::new();
-        find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result)?;
-        assert_eq!(result, b"lorem ipsum\n");
-        Ok(())
-    }
 }
