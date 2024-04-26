0708387c54403097f175f1b916ec89f171dbf78c
Martin Cajka <martin.cajka125@gmail.com>
2024-04-26 13:06:43 +0200

    initial implementation for multiple files checking

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,14 @@ fn main() -> Result<()> {
     env_logger::init();
     debug!("Start parsing cli args");
     let args = minigrep::Cli::parse();
-    debug!("{:?}", args);
+    debug!("{:?}", args.path);
 
-    let content = std::fs::read_to_string(&args.path)
-        .with_context(|| format!("Error reading file `{:?}`", &args.path))?;
+    for p in &args.path.unwrap() {
+        let content =
+            std::fs::read_to_string(p).with_context(|| format!("Error reading file `{:?}`", p))?;
 
-    minigrep::find_matches(&content, &args.pattern, &mut std::io::stdout())
-        .with_context(|| format!("Error querying the file `{:?}`", &args.path))?;
+        minigrep::find_matches(&content, &args.pattern, &mut std::io::stdout())
+            .with_context(|| format!("Error querying the file `{:?}`", p))?;
+    }
     Ok(())
 }
