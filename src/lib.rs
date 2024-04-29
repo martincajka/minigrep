fd9429f5ac55ac21d1c53d30efba08b7d4d488e3
Martin Cajka <martin.cajka125@gmail.com>
2024-04-29 16:11:42 +0200

    added multiple cli args

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,10 @@ pub struct Cli {
     pub count: Option<bool>,
     #[arg(short, long, action = clap::ArgAction::SetTrue)]
     pub line: Option<bool>,
+    #[arg(short='H', long, action = clap::ArgAction::SetTrue)]
+    pub heading: Option<bool>,
+    #[arg(short = 'C', long, default_value_t = 2)]
+    pub context: usize,
     pub pattern: String,
     pub path: Option<Vec<std::path::PathBuf>>,
 }
