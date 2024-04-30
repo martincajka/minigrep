e2a10d41839e52b4d447f7b2494cb28d3437faf6
Martin Cajka <martin.cajka125@gmail.com>
2024-04-30 15:53:25 +0200

    moved processing logic to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
+use anyhow::{Context, Result};
 use clap::{command, Parser};
+use std::io::Read;
 
 pub fn find_matches(
     content: &str,
     pattern: &str,
     mut writer: impl std::io::Write,
-) -> Result<(), std::io::Error> {
+) -> Result<(), anyhow::Error> {
     for line in content.lines() {
         if line.contains(pattern) {
             writeln!(writer, "{}", line)?;
@@ -28,14 +30,45 @@ pub struct Cli {
     pub path: Option<Vec<std::path::PathBuf>>,
 }
 
+impl Cli {
+    fn read_file(&self, path: &std::path::PathBuf) -> Result<String, anyhow::Error> {
+        let content: String = std::fs::read_to_string(path)
+            .with_context(|| format!("Error reading file `{:?}`", path))?;
+
+        Ok(content)
+    }
+
+    fn read_standard_input(&self) -> Result<String, anyhow::Error> {
+        let mut content = String::new();
+        std::io::stdin()
+            .read_to_string(&mut content)
+            .with_context(|| format!("Error reading standard input"))?;
+        Ok(content)
+    }
+
+    pub fn process_query(&self) -> Result<(), anyhow::Error> {
+        match &self.path {
+            Some(paths) => {
+                for p in paths {
+                    let content: String = self.read_file(p)?;
+                    find_matches(&content, &self.pattern, &mut std::io::stdout())?;
+                }
+            }
+            None => {
+                let content: String = self.read_standard_input()?;
+                find_matches(&content, &self.pattern, &mut std::io::stdout())?;
+            }
+        }
+        Ok(())
+    }
+}
 #[cfg(test)]
 mod tests {
-    use std::io;
 
     use crate::find_matches;
 
     #[test]
-    fn find_a_match() -> io::Result<()> {
+    fn find_a_match() -> Result<(), anyhow::Error> {
         let mut result = Vec::new();
         find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result)?;
         assert_eq!(result, b"lorem ipsum\n");
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,11 @@
-use std::io::Read;
-
-use anyhow::{Context, Result};
+use anyhow::Result;
 use clap::Parser;
 use log::debug;
 
 fn main() -> Result<()> {
     env_logger::init();
-    debug!("Start parsing cli args");
     let args = minigrep::Cli::parse();
     debug!("{:?}", args);
-
-    let mut content = String::new();
-    match args.path {
-        Some(paths) => {
-            for p in &paths {
-                content = std::fs::read_to_string(p)
-                    .with_context(|| format!("Error reading file `{:?}`", p))?;
-                minigrep::find_matches(&content, &args.pattern, &mut std::io::stdout())
-                    .with_context(|| format!("Error querying the file `{:?}`", p))?;
-            }
-        }
-        None => {
-            std::io::stdin().read_to_string(&mut content)?;
-            minigrep::find_matches(&content, &args.pattern, &mut std::io::stdout())
-                .with_context(|| "Error querying the standard input")?;
-        }
-    }
-
+    args.process_query()?;
     Ok(())
 }
