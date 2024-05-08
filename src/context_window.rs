268eb201200c642ade35032a7be3e896be970ba8
Martin Cajka <martin.cajka125@gmail.com>
2024-05-08 17:10:47 +0200

    new ContextView approach

--- src/context_window.rs
+++ src/context_window.rs
@@ -1,42 +1,27 @@
 use std::collections::VecDeque;
 
 pub struct ContextWindow {
-    context_lines: VecDeque<String>,
-    max_capacity: usize,
-    start_line_index: usize,
-    match_line_index: Option<usize>,
+    before_lines: VecDeque<String>,
+    matched_plus_after_lines: Vec<String>,
+    before_capacity_counter: usize,
+    after_capacity_counter: usize,
+    is_match: bool,
+    first_line: usize,
 }
 
 impl ContextWindow {
-    pub fn new(max_capacity: usize) -> Self {
-        Self {
-            context_lines: VecDeque::with_capacity(max_capacity),
-            max_capacity,
-            start_line_index: 0,
-            match_line_index: None,
+    pub fn new(before_capacity: usize, after_capacity: usize, first_line: usize) -> ContextWindow {
+        ContextWindow {
+            before_lines: VecDeque::with_capacity(before_capacity),
+            matched_plus_after_lines: Vec::new(),
+            before_capacity_counter: before_capacity,
+            after_capacity_counter: after_capacity,
+            is_match: false,
+            first_line,
         }
     }
 
-    pub fn add_line(&mut self, line: String, current_index: usize, is_match: bool) {
-        if self.context_lines.len() == self.max_capacity {
-            self.context_lines.pop_front();
-            self.start_line_index += 1;
-        }
-        self.context_lines.push_back(line);
-        if is_match {
-            self.match_line_index = Some(current_index);
-        }
-    }
-
-    pub fn get_lines(&self) -> Vec<String> {
-        self.context_lines.iter().cloned().collect()
-    }
-
-    pub fn get_start_line_index(&self) -> usize {
-        self.start_line_index
-    }
-
-    pub fn get_match_line_index(&self) -> Option<usize> {
-        self.match_line_index
+    pub fn add_line(&mut self) -> () {
+        todo!()
     }
 }
