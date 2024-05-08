fe81bf651dacbfaded0f0a225c451f3dbe69ec1c
Martin Cajka <martin.cajka125@gmail.com>
2024-05-08 19:08:30 +0200

    adding functionality for ContextWindow

--- src/context_window.rs
+++ src/context_window.rs
@@ -3,25 +3,71 @@ use std::collections::VecDeque;
 pub struct ContextWindow {
     before_lines: VecDeque<String>,
     matched_plus_after_lines: Vec<String>,
+    before_capacity: usize,
+    after_capacity: usize,
     before_capacity_counter: usize,
     after_capacity_counter: usize,
-    is_match: bool,
+    is_match_indices: Vec<usize>,
     first_line: usize,
 }
 
 impl ContextWindow {
-    pub fn new(before_capacity: usize, after_capacity: usize, first_line: usize) -> ContextWindow {
+    pub fn new(before_capacity: usize, after_capacity: usize) -> ContextWindow {
         ContextWindow {
             before_lines: VecDeque::with_capacity(before_capacity),
             matched_plus_after_lines: Vec::new(),
+            before_capacity,
+            after_capacity,
             before_capacity_counter: before_capacity,
             after_capacity_counter: after_capacity,
-            is_match: false,
-            first_line,
+            is_match_indices: Vec::new(),
+            first_line: 0,
         }
     }
 
-    pub fn add_line(&mut self) -> () {
+    pub fn add_line(&mut self, line: &str, is_match: bool) -> () {
         todo!()
+    }
+
+    pub fn finalize_after_last_line(&mut self, writer: impl std::io::Write) -> std::io::Result<()> {
+        if !self.is_match_indices.is_empty() {
+            return self.write(writer);
+        }
+        Ok(())
+    }
+
+    pub fn is_ready_to_write_out(&self) -> bool {
+        if !self.is_match_indices.is_empty()
+            && self.before_capacity_counter == 0
+            && self.after_capacity_counter == 0
+        {
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn write(&mut self, mut writer: impl std::io::Write) -> std::io::Result<()> {
+        for line in self.before_lines.iter() {
+            writeln!(writer, "{}", line)?;
+        }
+        for (i, line) in self.matched_plus_after_lines.iter().enumerate() {
+            if self
+                .is_match_indices
+                .contains(&(self.first_line + i + self.before_lines.len()))
+            {
+                writeln!(writer, "*{}", line)?; // mark matched lines with *
+            } else {
+                writeln!(writer, " {}", line)?; // mark context lines with space
+            }
+        }
+        self.first_line =
+            self.first_line + self.before_lines.len() + self.matched_plus_after_lines.len();
+        self.before_lines.clear();
+        self.matched_plus_after_lines.clear();
+        self.is_match_indices.clear();
+        self.before_capacity_counter = self.before_capacity;
+        self.after_capacity_counter = self.after_capacity;
+        Ok(())
     }
 }
